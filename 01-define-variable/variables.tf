b9e5562d8f2bb7d0170f19efa2a0e2b5910457e1
Muhammed Turde <50117470+mturde@users.noreply.github.com>
2022-09-28 09:02:41 +0300

    Add files via upload
    How to define a variable

---
+++ 01-define-variable/variables.tf
@@ -0,0 +1,38 @@
+#Provider
+
+variable "vsphere_server" {
+  default = "testvcenter" // vCenter Server FQDN or IP
+  description = "vSphere server"
+  type        = string
+}
+
+variable "vsphere_user" {
+  default = "testuser@vsphere.local"
+  description = "vSphere username"
+  type        = string
+  sensitive = true
+}
+
+variable "vsphere_password" {
+  default = "testpassword"
+  description = "vSphere password"
+  type        = string
+  sensitive   = true //if you don't want save variable to log
+}
+
+#Infrastructure
+variable "deploy_vsphere_datacenter" {
+  name = "dc-01"  
+}
+
+variable "deploy_vsphere_datastore" {
+  name = "datastore-01"
+}
+
+variable "deploy_vsphere_compute_cluster" {
+  name = "cluster-01"
+}
+
+variable "deploy_vsphere_network" {
+  name = "VM Network"
+}
