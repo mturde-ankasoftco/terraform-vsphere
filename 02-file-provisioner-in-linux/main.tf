de6a8b1e37353dca06f6bec7347f5ba57d5179a6
Muhammed Turde <50117470+mturde@users.noreply.github.com>
2022-10-05 14:00:08 +0300

    Add files via upload

---
+++ 02-file-provisioner-in-linux/main.tf
@@ -0,0 +1,17 @@
+resource "null_resource" fileProvisionerFolderInLinux {
+
+  connection {
+      type = "ssh"
+      user = var.admin_user
+      password = var.admin_password
+      host = var.vm_host
+      #you can also connect with ssh private key
+    }
+
+  ## Copy files to VM :
+  provisioner "file" {
+    source = "/home/muhammedturde/Desktop/script.ps1"
+    destination = "/home/${var.admin_user}"
+  }
+
+}
---
+++ 02-file-provisioner-in-linux/variables.tf
@@ -0,0 +1,7 @@
+
+#Informations
+variable "admin_user" { default = "root" }
+
+variable "admin_password" { default = "12345Terraform" }
+
+variable "vm_host" { name = "XXX.XXX.XXX.XXX" }
